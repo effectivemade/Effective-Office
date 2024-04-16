be190ce3a30842e97a08a2cf627a1c683a631a87
Slava <plumsoftwareofficial@gmail.com>
2024-04-16 19:38:31 +0600

    Random photos from album [~] Changed use case to return shuffled list with photos from album

--- tvApp/src/main/java/band/effective/office/tv/domain/use_cases/PhotoSynologyUseCase.kt
+++ tvApp/src/main/java/band/effective/office/tv/domain/use_cases/PhotoSynologyUseCase.kt
@@ -1,11 +1,24 @@
 package band.effective.office.tv.domain.use_cases
 
+import band.effective.office.tv.core.network.entity.Either
+import band.effective.office.tv.core.network.entity.map
+import band.effective.office.tv.domain.model.synology.PhotoDomain
 import band.effective.office.tv.repository.synology.PhotoSynologyRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class PhotoSynologyUseCase @Inject constructor(
     private val photoSynologyRepository: PhotoSynologyRepository
 ) {
-    suspend operator fun invoke(sid: String) =
-        photoSynologyRepository.getPhotosUrl(sid = sid)
+    suspend operator fun invoke(sid: String): Flow<Either<String, List<PhotoDomain>>> {
+        val eitherFlow: Flow<Either<String, List<PhotoDomain>>> =
+            photoSynologyRepository.getPhotosUrl(sid = sid)
+
+        eitherFlow.map { either ->
+            either.map { photos-> photos.shuffled()}
+        }
+
+        return eitherFlow
+    }
 }
